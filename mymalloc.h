94561b949ac7becfb386494fd84bc41487a563d3
Ammar Daskin <3620717+adaskin@users.noreply.github.com>
2024-03-30 07:56:08 +0300

    Update mymalloc.h

--- mymalloc.h
+++ mymalloc.h
@@ -17,9 +17,9 @@ typedef struct tag {
 } __attribute__((packed, aligned(16))) Tag; /* There is a padding*/
 
 typedef struct block {
+    Tag info;           /*size and isfree*/
     struct block *next; /*next free*/
     struct block *prev; /*next free*/
-    Tag info;           /*size and isfree*/
     char data[];        /*start of the allocated memory*/
 } __attribute__((packed, aligned(16))) Block;
 /*Block: |4byte(next)|4byte(prev)|8-byte size|4 byte isfree|0-byte(data)|*/
