7c67c9ad476b7f5949568fdeaf24d2d489d6c19d
Ammar Daskin <3620717+adaskin@users.noreply.github.com>
2024-03-30 07:52:22 +0300

    Update test.c

--- tests/test.c
+++ tests/test.c
@@ -1,1 +1,8 @@
 /*test.c sonradan eklenecek sizin bir sey yapmaniz gerekmiyor*/
+#include "../tests/mymalloc.h"
+#define studentmain main
+#include "../tests/mymalloc.c"
+#undef studentmain
+int main(){
+  return 0;
+}
